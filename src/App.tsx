22a14d6daa2a8b0800bcabc9a669b1984bbc7aa2
MyNameIsGustavo <gustavo.rochamaia@gmail.com>
2024-01-17 22:52:23 -0300

    feat:: criando as telas de registro de cliente e login

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { SignUp } from './paginas/admin/registro/registro-admin';
-
+//import { SignUp } from './paginas/admin/registro/registro-admin';
+//import { Login } from './paginas/paginaInicial-login';
+import { RegistroCliente } from "./paginas/cliente/registro-cliente"
 export const App = () => {
   return (
-    <SignUp />
+    <RegistroCliente />
   )
 }
--- src/paginas/cliente/registro-cliente.tsx
+++ src/paginas/cliente/registro-cliente.tsx
@@ -1,34 +1,19 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import SecurityIcon from '@mui/icons-material/Security';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { cadastraAdmin } from '../../../servicos/admin/servico-admin';
 
 const defaultTheme = createTheme();
 
-export function RegistroAdmin() {
+export function RegistroCliente() {
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-
-
-        /*const dados = {
-            user: data.get('user'),
-            nome: data.get('nome'),
-            sobrenome: data.get('sobrenome'),
-            cpf: data.get('cpf'),
-            senha: data.get('senha'),
-            codigoAdmin: data.get('codigoAdmin'),
-        };
-        await cadastraAdmin(dados);
-        */
     };
 
     return (
@@ -36,10 +21,7 @@ export function RegistroAdmin() {
             <Container component="main" maxWidth="xs">
                 <CssBaseline />
                 <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-
-                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}> <SecurityIcon /></Avatar>
-
-                    <Typography component="h1" variant="h5">Registre-se, administrador!</Typography>
+                    <Typography component="h1" variant="h5">Crie uma conta!</Typography>
 
                     <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
                         <Grid container spacing={2}>
@@ -64,17 +46,6 @@ export function RegistroAdmin() {
                                     fullWidth
                                 />
                             </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    name="user"
-                                    id="user"
-                                    label="Nome de usuario"
-                                    type='text'
-                                    required
-                                    fullWidth
-                                />
-                            </Grid>
-
 
                             <Grid item xs={12}>
                                 <TextField
@@ -84,6 +55,28 @@ export function RegistroAdmin() {
                                     label="CPF"
                                     type="text"
                                     id="cpf"
+                                />
+                            </Grid>
+
+                            <Grid item xs={12}>
+                                <TextField
+                                    name="endereco"
+                                    id="endereco"
+                                    label="Endereço"
+                                    type='text'
+                                    required
+                                    fullWidth
+                                />
+                            </Grid>
+
+                            <Grid item xs={12}>
+                                <TextField
+                                    name="celular"
+                                    id="celular"
+                                    label="Celular"
+                                    type='tel'
+                                    required
+                                    fullWidth
                                 />
                             </Grid>
 
@@ -107,16 +100,7 @@ export function RegistroAdmin() {
                                     id="reviewPassword"
                                 />
                             </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    name="codigoAdmin"
-                                    label="Código de admin"
-                                    type="password"
-                                    id="codigoAdmin"
-                                />
-                            </Grid>
+
                         </Grid>
                         <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }} fullWidth> Criar conta!</Button>
                         <Grid container justifyContent="center" marginBottom={3}>
